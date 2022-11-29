fb3629a875727e259356695b037e7269d7d252f9
C4-T0 <carlostorresh1@gmail.com>
2022-11-28 20:22:15 -0500

    .

--- pregunta.py
+++ pregunta.py
@@ -14,8 +14,28 @@ import pandas as pd
 
 def ingest_data():
 
-    #
-    # Inserte su código aquí
-    #
+    df=pd.read_table("clusters_report.txt",header=None)
+    df.drop([0,1,2],axis=0, inplace=True)
+
+    fila = {}
+    tabla=[]
+    for i in range(len(df)):
+        
+        line=df.iloc[i][0].strip().split()
+        if line[0].isnumeric():
+            if len(fila)>0:
+                tabla.append(fila)
+            fila={}
+            fila["cluster"]=int(line[0])
+            fila["cantidad_de_palabras_clave"]=int(line[1])
+            fila["porcentaje_de_palabras_clave"]=float(line[2].replace(",","."))
+            fila["principales_palabras_clave"]=" ".join(line[4:])
+        else:
+            fila["principales_palabras_clave"]+=" "+" ".join(line)
+            if fila["principales_palabras_clave"][-1]==".":
+                fila["principales_palabras_clave"]=fila["principales_palabras_clave"][:-1]
+    if len(fila) > 0:
+        tabla.append(fila)
+    df=pd.DataFrame(tabla)
 
     return df
